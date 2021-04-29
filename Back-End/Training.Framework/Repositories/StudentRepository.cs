67d8a342f234a03da1b305607d52da8c0d9d0690
Md Noyon Hossen <noyonhossen1996@gmail.com>
2021-04-29 23:45:41 +0600

    Student Repository added

---
+++ Back-End/Training.Framework/Repositories/StudentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Training.Data;
+using Training.Framework.Contexts;
+using Training.Framework.Entities;
+
+namespace Training.Framework.Repositories
+{
+    public class StudentRepository : Repository<Student, int, TrainingContext>, IStudentRepository
+    {
+        public StudentRepository(TrainingContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
