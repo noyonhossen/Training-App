51c8d4ac4b17f75af3f214b589a2c416adef380e
Md Noyon Hossen <noyonhossen1996@gmail.com>
2021-04-30 00:59:06 +0600

    Autofac configured

--- Back-End/Training.API/Training.API/Program.cs
+++ Back-End/Training.API/Training.API/Program.cs
@@ -1,11 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
 
@@ -43,6 +40,7 @@ namespace Training.API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
