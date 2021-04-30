739b94a431dba7f8734f0f831b4204c190ca122e
Md Noyon Hossen <noyonhossen1996@gmail.com>
2021-04-30 21:35:29 +0600

    course added,view and delete

--- Front-End/ClientApp/src/app/course/add-course/add-course.component.ts
+++ Front-End/ClientApp/src/app/course/add-course/add-course.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseService } from 'src/app/shared/course.service';
+import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
+import { Course } from 'src/app/shared/course.model';
 
 @Component({
   selector: 'app-add-course',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCourseComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service:CourseService,private toastr: ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm){
+    this.insertRecord(form);
+  }
+
+  insertRecord(form: NgForm){
+    this.service.postCourse().subscribe(
+      next=>{
+        this.resetForm(form);
+        this.toastr.success('Submitted successfully','Course Added successfully')
+      },
+      error=>{
+        console.log(error);
+      }
+    );
+  }
+
+  resetForm(form: NgForm)
+  {
+    form.form.reset();
+    this.service.formData = new Course();
   }
 
 }
--- Front-End/ClientApp/src/app/course/view-courses/view-courses.component.ts
+++ Front-End/ClientApp/src/app/course/view-courses/view-courses.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/shared/course.model';
+import { CourseService } from 'src/app/shared/course.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-view-courses',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewCoursesComponent implements OnInit {
 
-  constructor() { }
+  errorMessage:string;
+  courses: Course[];
+
+  constructor(public service: CourseService,private toastr: ToastrService) {
+    this.getCourses();
+  }
+
+  getCourses(){
+    this.service.getCourses().subscribe({
+      next: courses=>{
+          this.courses = courses;
+      },
+      error: err=>this.errorMessage = err
+    });
+  }
 
   ngOnInit(): void {
+    
+  }
+
+  onDelete(id:number)
+  {
+    if(confirm('Are you sure to delete this record?')){
+      this.service.deleteCourse(id).subscribe(
+        next=>{
+          this.getCourses();
+          this.toastr.error('Delete successfully','Course deleted successfully')
+        },
+        error=>{
+          console.log(error);
+        }
+      );
+    }
   }
 
 }
