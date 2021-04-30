087a47dc40307035d9b330d461658f94926b422e
Md Noyon Hossen <noyonhossen1996@gmail.com>
2021-04-30 22:35:48 +0600

    StudentRegistration crud

---
+++ Back-End/Training.Framework/Repositories/StudentRegistrationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Training.Data;
+using Training.Framework.Contexts;
+using Training.Framework.Entities;
+
+namespace Training.Framework.Repositories
+{
+    public class StudentRegistrationRepository : Repository<StudentRegistration, int, TrainingContext>, IStudentRegistrationRepository
+    {
+        public StudentRegistrationRepository(TrainingContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
---
+++ Back-End/Training.Framework/UnitOfWorks/IStudentRegistrationUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Training.Data;
+using Training.Framework.Repositories;
+
+namespace Training.Framework.UnitOfWorks
+{
+    public interface IStudentRegistrationUnitOfWork : IUnitOfWork
+    {
+        IStudentRegistrationRepository StudentRegistrationRepository { get; set; }
+    }
+}
