ad3822d157813d9f8880597f42420cc304ec7f06
Md Noyon Hossen <noyonhossen1996@gmail.com>
2021-05-01 10:00:20 +0600

    enrollment crud added

--- Front-End/ClientApp/src/app/enrollment/register/register.component.ts
+++ Front-End/ClientApp/src/app/enrollment/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { EnrollmentService } from 'src/app/shared/enrollment.service';
+import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
+import { Enrollment } from 'src/app/shared/enrollment.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service:EnrollmentService,private toastr: ToastrService,
+    private router: Router) {
+      
+     }
 
   ngOnInit(): void {
+    
+  }
+
+  onSubmit(form: NgForm){
+    this.insertRecord(form);
+  }
+
+  insertRecord(form: NgForm){
+    this.service.postEnrollment().subscribe(
+      next=>{
+        this.resetForm(form);
+        this.toastr.success('Submitted successfully','Enroll successfully');
+        this.router.navigate(['\view-registrations']);
+      },
+      error=>{
+        console.log(error);
+      }
+    );
+  }
+
+  resetForm(form: NgForm)
+  {
+    form.form.reset();
+    this.service.formData = new Enrollment();
   }
 
 }
--- Front-End/ClientApp/src/app/enrollment/view-registrations/view-registrations.component.ts
+++ Front-End/ClientApp/src/app/enrollment/view-registrations/view-registrations.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Enrollment } from 'src/app/shared/enrollment.model';
+import { EnrollmentService } from 'src/app/shared/enrollment.service';
+import { ToastrService } from 'ngx-toastr';
+import { StudentService } from 'src/app/shared/student.service';
 
 @Component({
   selector: 'app-view-registrations',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewRegistrationsComponent implements OnInit {
 
-  constructor() { }
+  errorMessage:string;
+  enrollments: Enrollment[];
+
+  constructor(public service: EnrollmentService,private toastr: ToastrService,
+    public studentService:StudentService) {
+    this.getEnrollments();
+  }
+
+  getEnrollments(){
+    this.service.getEnrollments().subscribe({
+      next: enrollments=>{
+          this.enrollments = enrollments;
+      },
+      error: err=>this.errorMessage = err
+    });
+  }
 
   ngOnInit(): void {
+    
+  }
+
+  onDelete(id:number)
+  {
+    if(confirm('Are you sure to delete this record?')){
+      this.service.deleteEnrollment(id).subscribe(
+        next=>{
+          this.getEnrollments();
+          this.toastr.error('Delete successfully','Enrollment deleted successfully')
+        },
+        error=>{
+          console.log(error);
+        }
+      );
+    }
   }
 
 }
